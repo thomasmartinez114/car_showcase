ece1b8fa6070f6fbd71c014b1eaebfabe860bc1f
Thomas Martinez <devtommie114@gmail.com>
2024-07-25 23:24:41 -0400

    started Footer // using constants data to render and populate footer links

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Footer, Navbar } from '@/components';
 
 export const metadata: Metadata = {
-  title: 'BTheRides',
+  title: 'CarHub',
   description: 'Discover the best cars in the world.',
 };
 
