b491061e866ad35329f5d28ad2f10b5ae1ba3f34
Thomas Martinez <devtommie114@gmail.com>
2024-07-26 23:34:58 -0400

    created search manufacturer comp

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+'use client';
+
+import { SearchManufacturer } from './';
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const handleSearch = () => {};
+
+  return (
+    <form onSubmit={handleSearch} className='searchbar'>
+      <div className='searchbar__item'>
+        <SearchManufacturer />
+      </div>
+    </form>
+  );
 };
 
 export default SearchBar;
