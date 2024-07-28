f5b62abc3dca022ba5fc640a4dd4afc3d20ebccd
Thomas Martinez <devtommie114@gmail.com>
2024-07-27 22:50:13 -0400

    styled combobox with color band when searching on bar

--- components/SearchMake.tsx
+++ components/SearchMake.tsx
@@ -64,7 +64,16 @@ const SearchMake = ({ make, setMake }: SearchMakeProps) => {
                     }
                     value={item}
                   >
-                    {item}
+                    {({ selected, active }) => (
+                      <>
+                        <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                          {item}
+                          </span>
+                          {selected ? (
+                            <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-teal-600'}`}>
+                            </span>) : null}
+                      </>
+                    )}
                   </Combobox.Option>
                 ))
               )}
