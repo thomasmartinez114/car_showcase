58bb125dc3b1083562ac696221eb0e50443fdacc
Thomas Martinez <devtommie114@gmail.com>
2024-07-29 00:07:11 -0400

    car default images displaying

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -27,6 +27,16 @@ const CarCard = ({ car }: CarCardProps) => {
         {carRent}
         <span className='self-end font-medium text-[14px]'>/day</span>
       </p>
+
+      <div className='relative w-full h-40 my-3 object-contain'>
+        <Image
+          src='/hero.png'
+          alt='car model'
+          fill
+          priority
+          className='object-contain'
+        />
+      </div>
     </div>
   );
 };
