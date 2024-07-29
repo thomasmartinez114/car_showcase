96030f12a8ad068226317b9d28768adab5b1168c
Thomas Martinez <devtommie114@gmail.com>
2024-07-28 23:58:29 -0400

    showing price per day on card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -22,8 +22,10 @@ const CarCard = ({ car }: CarCardProps) => {
         </h2>
       </div>
 
-      <p>
-        <span>{carRent}</span>
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px] font-semibold'>$</span>
+        {carRent}
+        <span className='self-end font-medium text-[14px]'>/day</span>
       </p>
     </div>
   );
