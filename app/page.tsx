da89ec46a6eac364bdc657b02fd86467f14f5245
Thomas Martinez <devtommie114@gmail.com>
2024-07-28 23:41:32 -0400

    show message on page if we have car data

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import Image from 'next/image';
 export default async function Home() {
   const allCars = await fetchCars();
 
-  console.log(allCars);
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  // console.log(allCars);
 
   return (
     <main className='overflow-hidden'>
@@ -25,6 +27,15 @@ export default async function Home() {
             <CustomFilter title='year' />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>WE HAVE CARS</section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
