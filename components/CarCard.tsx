542efcf9c8ae5f9dcc5a442e658c7fa62ceac48f
Thomas Martinez <devtommie114@gmail.com>
2024-07-30 23:05:08 -0400

    car details modal and car details rendering

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -74,7 +74,7 @@ const CarCard = ({ car }: CarCardProps) => {
             containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
             textStyles='text-white text-[14px] leading-[17px] font-bold'
             rightIcon='/right-arrow.svg'
-            handleClick={() => setisOpen(true)}
+            handleClick={() => setIsOpen(true)}
           />
         </div>
       </div>
