f86847eef7d46a609840b8e658f5eb89b6d9c1d1
Thomas Martinez <devtommie114@gmail.com>
2024-07-31 23:59:44 -0400

    carcard image

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { CarProps } from '@/types';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import CustomButton from './CustomButton';
 import CarDetails from './CarDetails';
 
@@ -35,7 +35,7 @@ const CarCard = ({ car }: CarCardProps) => {
       {/* Car Image */}
       <div className='relative w-full h-40 my-3 object-contain'>
         <Image
-          src='/hero.png'
+          src={generateCarImageUrl()}
           alt='car model'
           fill
           priority
