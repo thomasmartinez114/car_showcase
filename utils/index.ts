39c8affd5b77646b0e896805524d29830a29a6d1
Thomas Martinez <devtommie114@gmail.com>
2024-07-31 23:58:33 -0400

    api for car images set

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types';
+
 export async function fetchCars() {
   const headers = {
     'x-rapidapi-key': '64cadd98e2msh4dbf562a4bbc531p1443ffjsne0932b548394',
@@ -28,4 +30,21 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  // API Key here from imagin.studio
+  const url = new URL('https://cdn.imagin.studio/getimage');
+
+  const { make, year, model } = car;
+
+  // url.searchParams.append('customer', '<key here>');
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angel', `${angle}`);
+
+  return `${url}`;
 };
