897f893c40cf41d5b5b4db67ac868ccde0236c2a
Thomas Martinez <devtommie114@gmail.com>
2024-08-01 00:09:06 -0400

    added car angles

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -76,7 +76,7 @@ const CarDetails = ({
                     <div className='flex gap-3'>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                         <Image
-                          src={generateCarImageUrl(car, 'angle')}
+                          src={generateCarImageUrl(car, '29')}
                           alt='car model'
                           fill
                           priority
@@ -85,7 +85,7 @@ const CarDetails = ({
                       </div>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                         <Image
-                          src={generateCarImageUrl(car, 'angle')}
+                          src={generateCarImageUrl(car, '33')}
                           alt='car model'
                           fill
                           priority
@@ -94,7 +94,7 @@ const CarDetails = ({
                       </div>
                       <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                         <Image
-                          src={generateCarImageUrl(car, 'angle')}
+                          src={generateCarImageUrl(car, '13')}
                           alt='car model'
                           fill
                           priority
