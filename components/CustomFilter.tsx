5fe473ac678679204e80caffaa8ba37e99f97250
Thomas Martinez <devtommie114@gmail.com>
2024-08-01 23:40:24 -0400

    updated search params and render from filter // made reusuable updateSearchParams wihtin utils/index.ts

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,13 +5,28 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Listbox, Transition } from '@headlessui/react';
 import { CustomFilterProps } from '@/types';
+import { updateSearchParams } from '@/utils';
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
+
+  // update params of search bar
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
 
   return (
     <div className='w-fit'>
-      <Listbox value={selected} onChange={e => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={e => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
         <div className='relative w-fit z-10'>
           <Listbox.Button className='custom-filter__btn'>
             <span className='block truncate'>{selected.title}</span>
